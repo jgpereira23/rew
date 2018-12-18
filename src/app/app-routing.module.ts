f42077dd1787de9023c967262ba3faa4c466acb3
Jose Gonzalez <jgpereira23@gmail.com>
2018-12-18 13:26:49 -0500

    Add login, signup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateListingComponent } from './listings/create-listing/create-listing.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 const routes: Routes = [
-  { path: '', component: CreateListingComponent }
+  { path: '', component: CreateListingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
