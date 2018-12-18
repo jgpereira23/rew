6c793a1affbdd05346e215b98c266effacd8a7d4
Jose Gonzalez <jgpereira23@gmail.com>
2018-12-18 13:27:35 -0500

    Add login page

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      'email': new FormControl(),
+      'password': new FormControl()
+    });
+  }
+
+  onLogin() {
+
+  }
+}
