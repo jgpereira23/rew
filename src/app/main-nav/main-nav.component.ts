4f6127f201a03c4e147bd850aa9b40d305954472
Jose Gonzalez <jgpereira23@gmail.com>
2018-12-20 12:55:24 -0500

    Enable logout functionality

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,29 +1,52 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit, OnDestroy } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map, filter, withLatestFrom } from 'rxjs/operators';
 import { Router, NavigationEnd } from '@angular/router';
 import { MatSidenav } from '@angular/material';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css'],
+  styleUrls: ['./main-nav.component.css']
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit, OnDestroy {
   @ViewChild('drawer') drawer: MatSidenav;
+  userIsAuthenticated = false;
+  private authListenenrSubs: Subscription;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
 
-  constructor(private breakpointObserver: BreakpointObserver, private router: Router) {
-    router.events.pipe(
-      withLatestFrom(this.isHandset$),
-      filter(([a, b]) => b && a instanceof NavigationEnd)
-    ).subscribe(_ => this.drawer.close());
-
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    router.events
+      .pipe(
+        withLatestFrom(this.isHandset$),
+        filter(([a, b]) => b && a instanceof NavigationEnd)
+      )
+      .subscribe(_ => this.drawer.close());
   }
 
+  ngOnInit() {
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authListenenrSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+      });
+  }
+
+  onLogout() {
+    this.authService.logoutUser();
+  }
+
+  ngOnDestroy () {
+    this.authListenenrSubs.unsubscribe();
+  }
 }
