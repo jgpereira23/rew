1929533856bc72bbef871739fbe4d3d5cab5d021
Jose Gonzalez <jgpereira23@gmail.com>
2018-12-19 19:10:45 -0500

    Add create listings skeleton

--- src/app/listings/create-listing/create-listing.component.ts
+++ src/app/listings/create-listing/create-listing.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create-listing',
   templateUrl: './create-listing.component.html',
   styleUrls: ['./create-listing.component.css']
 })
-export class CreateListingComponent {
+export class CreateListingComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      'title': new FormControl(),
+      'content': new FormControl(),
+      'image': new FormControl()
+    });
+  }
 
 }
