8dcadd011d98c5219bebdb0728da332c8c4848cd
Jose Gonzalez <jgpereira23@gmail.com>
2018-12-19 19:12:42 -0500

    Add login and signup routes

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+const env = process.env.NODE_ENV || "development";
+const config = require("./config")[env];
+
+mongoose
+  .connect(
+    config.database.prefix +
+      config.database.username +
+      ":" +
+      config.database.password +
+      config.database.domain +
+      config.database.db +
+      config.database.params
+  )
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+// xPsJz8t3Fq9eXiiw
+
+app.use("/api/user", userRoutes);
+
+module.exports = app;
