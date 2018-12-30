b2e28586b6df807a58b3d51c96fc33a96cd750c4
Jose Gonzalez <jgpereira23@gmail.com>
2018-12-30 15:04:18 -0500

    Add form inputs for listing values

--- src/app/listings/create-listing/create-listing.component.ts
+++ src/app/listings/create-listing/create-listing.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { ImageComponent } from 'src/app/image-dialog/image.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-create-listing',
@@ -8,13 +10,52 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class CreateListingComponent implements OnInit {
   form: FormGroup;
+  imagePreviews: string[] = [];
+  files: File[] = [];
+  @ViewChild('filePicker') filePicker;
+
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      'title': new FormControl(),
-      'content': new FormControl(),
-      'image': new FormControl()
+      'streetAddress': new FormControl(),
+      'city': new FormControl(),
+      'zipCode': new FormControl(),
+      'homeSquareFeet': new FormControl(),
+      'landSquareFeet': new FormControl(),
+      'yearBuilt': new FormControl(),
+      'propertyType': new FormControl(),
+      'saleType': new FormControl(),
+      'bathrooms': new FormControl(),
+      'bedrooms': new FormControl(),
+      'price': new FormControl(),
+      'finishedBasement': new FormControl()
     });
   }
 
+  onImagesSelected(event: Event) {
+    const files = Array.from((event.target as HTMLInputElement).files);
+    files.forEach( (file) => {
+      this.form.patchValue({
+        image: file
+      });
+      this.form.get('image').updateValueAndValidity();
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.files = this.files.concat(files);
+        this.imagePreviews.push(reader.result.toString());
+      };
+      reader.readAsDataURL(file);
+    });
+    console.log(this.imagePreviews);
+  }
+
+  onImageRemoved(imageIndex: number) {
+    this.files.splice(imageIndex, 1);
+    this.imagePreviews.splice(imageIndex, 1);
+  }
+
+  onImageView(imageIndex: number) {
+    this.dialog.open(ImageComponent, {data: {imageLink: this.imagePreviews[imageIndex]}});
+  }
 }
